b79c7000b1ae5342dfefe40e9c580d87efc58528
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-03-01 22:45:59 +0530

    set bottom padding for tabs

--- mobile/components/SafeScreen.tsx
+++ mobile/components/SafeScreen.tsx
@@ -6,7 +6,7 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context'
 const SafeScreen = ({children}: {children: React.ReactNode}) => {
     const insets=useSafeAreaInsets();
   return (
-    <View style={{paddingTop:insets.top,flex:1}}>
+    <View style={{paddingTop:insets.top,paddingBottom:insets.bottom,flex:1}}>
      {children}
     </View>
   )
